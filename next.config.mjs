5ec79b2f32306c1c98d2b6c5120685dcb8368f0a
Tyler Bert Baring <tbaring_ccs@uspf.edu.ph>
2024-09-25 17:04:19 +0800

    Fix: Update nextConfig

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    webpack: (config, { isServer }) => {
+      config.externals.push({
+        'utf-8-validate': 'commonjs utf-8-validate',
+        'bufferutil': 'commonjs bufferutil',
+        'zlib-sync': 'commonjs zlib-sync',
+      });
+      return config;
+    },
+  };
+  
+  export default nextConfig;
