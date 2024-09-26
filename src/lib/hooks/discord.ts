73c99bd58a784212e555452f546998f0ac662e62
Tyler Bert Baring <tbaring_ccs@uspf.edu.ph>
2024-09-26 08:53:36 +0800

    Revert "STG: Test Push"
    This reverts commit 8198f7603ecaca76f0739bfaa0aff2dac06897cc.

--- src/lib/hooks/discord.ts
+++ src/lib/hooks/discord.ts
@@ -49,6 +49,7 @@ export default async function sendDiscordMessage(channelId: string, message: str
             console.error('Stack trace:', error.stack);
         }
     } finally {
+        console.log('Destroying client connection');
         await client.destroy();
     }
 }
