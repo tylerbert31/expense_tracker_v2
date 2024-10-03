0dd561e313eda864ac222077b563dda587f43d74
Tyler Bert Baring <tbaring_ccs@uspf.edu.ph>
2024-10-03 11:30:32 +0800

    Fix: Temporary - No Draw Percentage

--- src/app/api/cron/lunch/route.ts
+++ src/app/api/cron/lunch/route.ts
@@ -18,42 +18,24 @@ export async function POST() {
     .then((res) => res.json())
     .then((data) => data.items[0].choice);
 
-
-  const lunchChoices = await fetch(
-    `${FDC_PB_URL}/fdc_lunch_choices/records?filter=(id!='${prevLunch}')&fields=id,name,weight`
-  )
-    .then((res) => res.json())
-    .then((data) => data.items);
-
-
-  const totalWeight = lunchChoices.reduce(
-    (sum: any, choice: any) => sum + choice.weight,
-    0
-  );
-  let randomWeight = Math.random() * totalWeight;
-
-  const selectedChoice = lunchChoices.find((choice: any) => {
-    if (randomWeight < choice.weight) {
-      return true;
-    }
-    randomWeight -= choice.weight;
-    return false;
-  });
+  const random_beta = await axios.get(`${FDC_PB_URL}/random_lunch_beta/records`)
+    .then((res) => res.data.items[0]);
 
   const today = new Date();
   const formattedDate = today.toISOString().split('T')[0];
 
   message += '\n```Date: ' + formattedDate;
-  message += "\nToday's Lunch: " + selectedChoice.name + "```";
+  message += "\nToday's Lunch: " + random_beta.name + "```";
 
   try {
-    await sendDiscordMessage("1287663632859140147", message);
-
-    if(process.env.ENVIRONMENT == "PRODUCTION") {
-      await axios.post(`${FDC_PB_URL}/fdc_prev_lunch/records`, {
-        choice: selectedChoice.id
+    await sendDiscordMessage("1287663632859140147", message)
+      .then(async () => {
+        if(process.env.ENVIRONMENT == "PRODUCTION") {
+          await axios.post(`${FDC_PB_URL}/fdc_prev_lunch/records`, {
+            choice: random_beta.id
+          });
+        }
       });
-    }
   } catch (error) {
     //
   }
