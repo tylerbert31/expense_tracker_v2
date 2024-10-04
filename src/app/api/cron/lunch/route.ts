2370d89e0cf807a2ba60a46ffe6860d7dc0e824b
Tyler Bert Baring <tbaring_ccs@uspf.edu.ph>
2024-10-04 08:34:17 +0800

    Feat: Cron-Lunch Pater Friday

--- src/app/api/cron/lunch/route.ts
+++ src/app/api/cron/lunch/route.ts
@@ -1,38 +1,52 @@
 import sendDiscordMessage from "@/lib/hooks/discord";
 import { NextResponse } from "next/server";
 import axios from "axios";
+import { format } from "date-fns";
 
 const FDC_PB_URL = process.env.FDC_PB_URL;
 const devs_url = `${FDC_PB_URL}/fdc_devs/records?fields=discord_id`;
 
 export async function POST() {
-  const userIds = await fetch(devs_url)
-    .then((res) => res.json())
-    .then((data) => data.items.map((dev: any) => dev.discord_id));
+  let userIds = [];
 
-  let message = `<@${userIds.join("> <@")}>`;
+  if(process.env.ENVIRONMENT == "PRODUCTION"){
+    userIds = await fetch(devs_url)
+      .then((res) => res.json())
+      .then((data) => data.items.map((dev: any) => dev.discord_id));
+  } else {
+    userIds = ["680040840592490534"];
+  }
 
-  const prevLunch = await fetch(
-    `${FDC_PB_URL}/fdc_prev_lunch/records?sort=-created&perPage=1&fields=choice`
-  )
-    .then((res) => res.json())
-    .then((data) => data.items[0].choice);
-
-  const random_beta = await axios.get(`${FDC_PB_URL}/random_lunch_beta/records`)
-    .then((res) => res.data.items[0]);
-
+  const dayNum = Number(format(new Date(), 'i'));
   const today = new Date();
   const formattedDate = today.toISOString().split('T')[0];
 
+  let random_beta = {
+    id: null,
+    name: null
+  };
+
+
+  let message = `<@${userIds.join("> <@")}>`;
   message += '\n```Date: ' + formattedDate;
-  message += "\nToday's Lunch: " + random_beta.name + "```";
+
+  if(dayNum !== 5){
+    random_beta = await axios.get(`${FDC_PB_URL}/random_lunch_beta/records`)
+    .then((res) => res.data.items[0]);
+
+    message += "\nToday's Lunch: " + random_beta.name + "```";
+  } else {
+    message += "\nToday's Lunch: Pater Friday!```";
+  }
 
   try {
     await sendDiscordMessage("1287663632859140147", message)
       .then(async () => {
-        if(process.env.ENVIRONMENT == "PRODUCTION") {
+        if(process.env.ENVIRONMENT == "PRODUCTION" && dayNum !== 5){
           await axios.post(`${FDC_PB_URL}/fdc_prev_lunch/records`, {
             choice: random_beta.id
+          }).then((res) => {
+            console.log(res.data);
           });
         }
       });
